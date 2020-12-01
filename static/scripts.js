23566cfda3a1dc40749c305a4ff8e6bb10e98494
sarahtan931 <sarah.tan931@gmail.com>
2020-11-30 21:47:34 -0500

    more backend functionality

--- static/scripts.js
+++ static/scripts.js
@@ -4,64 +4,115 @@ var mysql = require('mysql');
 const express = require('express'); 
 const app = express(); 
 const mssql = require("mssql"); 
-  
-// Get request 
-app.get('/', function (req, res) { 
-  
-    var con = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "new_password",
-        port: 3306,
-        database: "lodger"
-    });
 
-    con.connect(function(err) {
-        if (err) throw err;
-    console.log("Connected!");
-   // });
-    // Connect to your database 
-    //mssql.connect(config, function (err) { 
-  
-        // Create Request object to preform 
-        // query operation 
-       // var request = new mysql.Request(); 
-        con.query("SELECT * FROM landlord", (err, res) => {
-            if (err) {
-              console.log("error: ", err);
-             // result(null, err);
-             //turn;
-            }
-            else{
-        
-            if(res){
-                console.log("landlord: ", res);
-               
-            }
-            //result(null, res);
-            
-            }
-          });
-  
-        
-          // Query to the database and get the records 
-      
-    }); 
-}); 
-  
-var server = app.listen(3000, function () { 
-    console.log('Server is listening at port 5000...'); 
-}); 
-
-/*
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "new_password",
-  port: 3306
+    host: "localhost",
+    user: "root",
+    password: "new_password",
+    port: 3306,
+    database: "lodger",
+    multipleStatements: true
+});
+//make connection
+con.connect(function(err) {
+    if (err) throw err;
+console.log("Connected!");
+}); 
+
+//login landlord
+app.get('/loginlandlord/:email/:password', function (req, res) { 
+    console.log("Connected!");
+    email = req.params.email;
+    password = req.params.password;
+ 
+con.query("SELECT name FROM landlord WHERE email = ? AND password = ?", [email, password], (error, results) => {
+    if (error) {
+        console.log("error: ", error);
+    }
+    console.log(results);
+    res.send(results);
+    })
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});*/
+//login tentant
+app.get('/logintenant/:email/:password', function (req, res) {  
+     email = req.params.email;
+     password = req.params.password;
+  
+ con.query("SELECT * FROM tenant WHERE email = ? AND password = ?", [email, password], (error, results) => {
+     if (error) {
+         console.log("error: ", error);
+     }
+     console.log(results);
+     res.send(results);
+     })
+ }); 
+
+ //search all landlord properties and update them
+app.put('/updateproperties/:price/:num', function (req, res) {  
+    price = req.params.price;
+    num = req.params.num;
+con.query(`UPDATE property SET price = ${price} WHERE accountNumL = ${num} `, (error, results) => {
+    if (error) {
+        console.log("error: ", error);
+    }
+    console.log(results);
+    res.send(results);
+    })
+}); 
+
+//find a property and add a review 
+app.put('/addreview', function(req,res){
+    let name = "Nola N. Valdez";
+    let numOfStars = 7;
+    let date = '2020-07-11';
+    
+    con.query((`SELECT * FROM propertyRental p WHERE p.accountNumT = (SELECT accountNumT FROM tenant t WHERE t.name = '${name}')`), (error, results) => {
+        if (error) {
+            console.log("error: ", error);
+        }
+        let propertyNum = results[0].propertyNum;
+        con.query((`SELECT  loanNum FROM propertyrental p WHERE p.propertyNum=${propertyNum}` ), (error1, results1) => {
+            if (error1){
+                console.log(error1);
+            }
+            let loanNum = results1[0].loanNum;
+            con.query((`INSERT INTO review VALUES(${loanNum},'${name}','${date}',${numOfStars})`), (error2, results2) =>{
+                if (error2){
+                    console.log(error2)
+                }
+                console.log(results2)
+                res.send(results2)
+            })
+
+        })
+        })
+})
+
+app.post('/addamenity', function(req,res){
+    let landlordNum = 3;
+    let amenity = "Sauna"
+
+    con.query((`SELECT propertyNum 
+    FROM property
+    WHERE price > 200 AND accountNumL = ${landlordNum}`), (error, results) =>{
+        if (error) {
+            console.log("error: ", error);
+        }
+        let propertyNum = results[0].propertyNum;
+        console.log(propertyNum)
+        con.query((`INSERT INTO amenities VALUES('${amenity}', ${propertyNum})`), (error1, results1) => {
+            if(error1){
+                console.log("error: ", error1);
+            }
+            console.log(results1);
+            res.send(results1)
+        })
+    })
+    
+})
+
+var server = app.listen(3000, function () { 
+    console.log('Server is listening at port 3000...'); 
+}); 
+
