28a3bac5e1547c9c56ef93525735951b6560a2d7
Kylie Holman <kholma@uwo.ca>
2020-11-30 19:47:14 -0500

    started middleware

---
+++ static/index.js
@@ -0,0 +1,49 @@
+//Landlord login
+function landlordLogin(landlordEmail,landlordPassword){
+    fetch('/api/loginlandlord/'+landlordEmail+'/'+landlordPassword)
+    .then(
+        function(response){ 
+           if(response){
+//load landlord.html
+           }
+           else{
+//load Login.html
+           }
+      }
+    )
+  .catch(function(error){
+  console.log("Error:", error);
+    });
+  
+
+}
+
+//Tenant login
+function tenantLogin(tenantEmail,tenantPassword){
+    fetch('/api/logintenant/'+tenantEmail+'/'+tenantPassword)
+    .then(
+        function(response){ 
+           if(response){
+//load tenant.html
+           }
+           else{
+//load login.html
+           }
+      }
+    )
+  .catch(function(error){
+  console.log("Error:", error);
+    });
+
+}
+
+//Landlord-Search all properties 
+function searchLProperties(landlordName){
+
+
+}
+
+//Landlord-Update a property 
+function updateLProperty(propertyNum){
+
+}
